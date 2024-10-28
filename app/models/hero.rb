cbfd76238ef9d87b8f2943b766d3cb9d8833b80e
Lucas Perin <lucasdafonsecaperin@gmail.com>
2024-10-28 19:16:05 -0300

    Adiciona testes no model Hero

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,6 @@
 class Hero < ApplicationRecord
-  validates :name, :token, presence: true
-  validates :name, uniqueness: { scope: :token, case_sensitive: false }
+  validates :name, presence: true, uniqueness: { scope: :token, case_sensitive: false }
+  validates :token, presence: true, length: { minimum: 10 }
 
   scope :by_token, ->(token) { where(token: token) }
   scope :sorted_by_name, -> { order(:name) }
